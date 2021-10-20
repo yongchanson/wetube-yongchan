45417cb3670d76de76470b0ae0d8ab7cc641036d
yongchanson <syc9430@gmail.com>
2021-10-21 03:55:17 +0900

    init.js

--- src/init.js
+++ src/init.js
@@ -12,6 +12,18 @@ const PORT = process.env.PORT || 4000;
 const handleListening = () => 
 console.log(`✅Server listenting on http://localhost:${PORT}🚀`);
 
-app.listen(PORT, handleListening);
+// app.listen(PORT, handleListening);
+app.listen(PORT, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+
+//https://wetube-yongchan.herokuapp.com/
 
 //tc1iofCbUMBVNChx
+//mongodb+srv://yongchan:tc1iofCbUMBVNChx@cluster0.ybfcy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+//9RL8DulupnQr6jQQ
+//mongodb+srv://yongchan:9RL8DulupnQr6jQQ@cluster0.1mm3h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+//heroku login
+//heroku git:remote -a wetube-yongchan
+
