9ba16f3a800ed20716cd76f8b21e1ef9a3256d84
yongchanson <syc9430@gmail.com>
2021-10-22 20:32:28 +0900

    router contorller

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -144,3 +144,25 @@ export const createComment = async (req, res) => {
   video.save();
   return res.status(201).json({ newCommentId: comment._id });
 };
+
+export const deleteComment = async (req, res) => {
+  const {
+    session: { user },
+    params: { id },
+  } = req;
+  const comment = await Comment.findById(id);
+  if (!comment) {
+    return res.sendStatus(404);
+  }
+  const video = await Video.findById(comment.video);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  if (user._id != comment.owner) {
+    return res.sendStatus(404);
+  }
+  await Comment.findByIdAndDelete(id);
+  video.comments.remove(id);
+  video.save();
+  return res.sendStatus(201);
+};
